d4af8a578b0c5c3932995fff5e2421c0423bbd45
SYJ <maplesyj@gmail.com>
2017-03-29 12:48:17 -0700

    use modal form to upload, try to use dropzone

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+(function(window) {
+    'use strict';
+    var FORM_SELECTOR = '[data-coffee-order="form"]';
+    var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
+    //var SERVER_URL = 'http://coffeerun-v2-rest-api.herokuapp.com/api/coffeeorders';
+    var SERVER_URL = 'http://localhost:3002/coffeeorders';
+    var App = window.App;
+    var Photo = App.Photo;
+
+    var myphoto = new Photo('#create');
+    myphoto.addClickHandler();
+})(window);
