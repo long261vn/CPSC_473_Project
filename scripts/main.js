ad108962284ee3303cca55d1289efc467cb0b198
Kevin Lam <vertepenguin@csu.fullerton.edu>
2017-03-29 14:38:38 -0700

    Display feeds - still inprogress

--- scripts/main.js
+++ scripts/main.js
@@ -4,9 +4,11 @@
     var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
     //var SERVER_URL = 'http://coffeerun-v2-rest-api.herokuapp.com/api/coffeeorders';
     var SERVER_URL = 'http://localhost:3002/coffeeorders';
+
     var App = window.App;
     var Photo = App.Photo;
 
     var myphoto = new Photo('#create');
+
     myphoto.addClickHandler();
 })(window);
