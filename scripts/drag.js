bcbfbbbf933fd68591d994a1c2d16069fd995d00
long261vn <long261vn@yahoo.com>
2017-04-03 19:32:16 -0700

    Update Search. Pic result will show up now

--- scripts/drag.js
+++ scripts/drag.js
@@ -94,7 +94,7 @@
     File.prototype.convertToBase64 = function(callback){
             var reader = new FileReader();
             reader.onload = function(e) {
-                 callback(e.target.result)
+                 callback(e.target.result);
             };
             reader.onerror = function(e) {
                  callback(null);
