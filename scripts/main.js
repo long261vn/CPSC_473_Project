b6d67f22f6c32ebcdd1d94c70fff2edf40c3f5d2
Tushar Kamble <tushark26@csu.fullerton.edu>
2017-04-06 22:54:13 -0700

    Updated Main.js
    Dynamic image retrieval and display is implemented and also removed the hard coded images

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,188 @@
-(function(window) {
-    'use strict';
-    var FORM_SELECTOR = '[data-coffee-order="form"]';
-    var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
-    //var SERVER_URL = 'http://coffeerun-v2-rest-api.herokuapp.com/api/coffeeorders';
-    var SERVER_URL = 'http://localhost:3002/coffeeorders';
-    var App = window.App;
-    var Photo = App.Photo;
+/*eslint-disable no-unused-vars*/
 
-    var myphoto = new Photo('#create');
-    myphoto.addClickHandler();
-})(window);
+// function readImage(inputElement) {
+//     var deferred = $.Deferred();
+//
+//     var files = inputElement.get(0).files;
+//     if (files && files[0]) {
+//         var fr= new FileReader();
+//         fr.onload = function(e) {
+//             deferred.resolve(e.target.result);
+//         };
+//         fr.readAsDataURL( files[0] );
+//     } else {
+//         deferred.resolve(undefined);
+//     }
+//
+//     return deferred.promise();
+// }
+//
+// $("#input").on('change',function(){
+//     readImage($(this)).done(function(base64Data){
+//         //alert(base64Data);
+//         var info = $("#info").val();
+//         var pic = base64Data;
+//         upload(pic, info);
+//     });
+// });
+//
+// function upload(pic, info) {
+//     $.ajax
+//     ({
+//         type: "POST",
+//         //the url where you want to sent the userName and password to
+//         url: "http://localhost:3002/pics",
+//         dataType: 'json',
+//         async: false,
+//         //json object to sent to the authentication url
+//         data: {"pic": pic, "info" : info},
+//         success: function () {
+//             //do any process for successful authentication here
+//             alert("Upload successfully!");
+//
+//         }
+//     })
+// }
+
+// =============================== Signup ===============================
+$(document).ready(function() {
+    $("#btnSignup").click(function() {
+        //collect userName and password entered by users
+        var userName = $("#username").val();
+        var passWord = $("#password").val();
+        var email = $("#email").val();
+        var error = true;
+        //call the authenticate function
+        $.ajax({
+            type: "POST",
+            //the url where you want to sent the userName and password to
+            url: "http://localhost:3002/signup",
+            dataType: 'json',
+            async: false,
+            //json object to sent to the authentication url
+            data: {
+                "username": userName,
+                "password": passWord,
+                "email": email
+            },
+            success: function() {
+                //do any process for successful authentication here
+                alert("You have successfully signed up! Please log-in to upload pictures");
+            }
+        });
+    });
+});
+
+
+// =============================== Login ===============================
+$(document).ready(function() {
+    //event handler for login button
+    $("#btnLogin").click(function() {
+        //collect userName and password entered by users
+        var user = $("#username").val();
+        var pass = $("#password").val();
+        var error = true;
+
+        $.ajax({
+            type: "GET",
+            url: "http://localhost:3002/signup",
+            dataType: 'json',
+            success: function(data) {
+                $.each(data, function(key, value) {
+                    if (user == value.username && pass == value.password) {
+                        error = false;
+                    }
+                });
+
+                if (error == false) {
+                    alert("You have successfully logged in!");
+                    document.location = "upload.html";
+                } else {
+                    $("#username").val('');
+                    $("#password").val('');
+                    alert("wrong user or password!");
+                }
+            }
+        });
+
+        return false;
+    });
+});
+
+// =============================== Read pics and display to screen ===============================
+$(document).ready(function() {
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:3002/pics",
+        dataType: 'json',
+        success: function(data) {
+            var i = 0;
+            $.each(data, function(key, value) {
+                $("#ul").append("<li class='myff' id='" + i + "' onclick='hello(this.id)'><p>Title: " + value.info + "</p><img src=" + value.pic + " id=\"image\"/><br /><br /></li>")
+                i++;
+            });
+        }
+    });
+});
+
+// ===================================================Modal Display of Images=============================================
+
+function hello(id) {
+    console.log("Hello World");
+    console.log(id);
+    var ii2 = document.getElementById("ii2");
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:3002/pics",
+        dataType: 'json',
+        success: function(data) {
+            $.each(data, function(key, value) {
+                if (key == id) {
+                    var image = new Image();
+                    image.src = value.pic;
+                    $("#myModal").modal('show');
+                    ii2.appendChild(image);
+                    info.innerHTML = value.info;
+                    title.innerHTML = value.info;
+
+                    $('#myModal').on('hidden.bs.modal', function() {
+                        ii2.innerHTML = "";
+                        //  $('ul').on();
+                    });
+                }
+            });
+        }
+    });
+}
+
+
+/*
+$(document).ready(function() {
+    //event handler for login button
+    var ii2 = document.getElementById("ii2");
+    $('ul').on('click', function() {
+        $.ajax({
+            type: "GET",
+            url: "http://localhost:3002/pics",
+            dataType: 'json',
+            success: function(data) {
+                $.each(data, function(key, value) {
+                    var image = new Image();
+                    image.src = value.pic;
+                    $("#myModal").modal('show');
+                    ii2.appendChild(image);
+                    info.innerHTML = value.info;
+                    title.innerHTML = value.info;
+
+                    $('#myModal').on('hidden.bs.modal', function() {
+                        ii2.innerHTML = "";
+                        $('ul').on();
+                    });
+
+                });
+            }
+        });
+    });
+});
+
+*/
